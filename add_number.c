eebd0285435a58bf5c5dd19afefeffe7ecbea31a
janyuaru <139055982+janyuaru@users.noreply.github.com>
2024-07-25 10:13:40 +0700

    Add files via upload

--- add_number.c
+++ add_number.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
 void main(){
     int x, y, z;
-    scanf("%d", &x);
-    printf("%d", x%10);
+    scanf("%d%d%d", &x, &y, &z);
+    //SOL1
+    int result = x+y+z;
+    printf("%d", result);
+    //SOL2
+    //printf("%d", x+y+z);
 }
